abc4043be7d8961c0e7e02618373dc4c4169aaee
Vincent Batts <vbatts@hashbangbash.com>
2012-06-19 23:36:10 -0400

    now it runs in the most elemtary way!

--- app.rb
+++ app.rb
@@ -8,4 +8,25 @@ ActiveRecord::Base.logger = Logger.new('log/debug.log')
 ActiveRecord::Base.configurations = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection('development')
 
+module Gnucash
+  class Accounts < ActiveRecord::Base; end
+  class Books < ActiveRecord::Base; end
+  class BudgetAmounts < ActiveRecord::Base; end
+  class Commodities < ActiveRecord::Base; end
+  class Recurrences < ActiveRecord::Base; end
+  class Schedxactions < ActiveRecord::Base; end
+  class Slots < ActiveRecord::Base; end
+  class Splits < ActiveRecord::Base; end
+  class Transactions < ActiveRecord::Base; end
+  class Versions < ActiveRecord::Base; end
+end
 
+if $0 == __FILE__
+  a = Gnucash::Transactions.find :all, :limit => 5
+  a.each {|x|
+    p x
+    puts
+  }
+end
+
+# vim: set sts=2 sw=2 et ai:
