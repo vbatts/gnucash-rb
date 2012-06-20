7a510e6dae5174c9de8441230a9e39e62ccde394
Vincent Batts <vbatts@hashbangbash.com>
2012-06-20 00:16:01 -0400

    adding comments and license

--- app.rb
+++ app.rb
@@ -1,4 +1,32 @@
 #!/usr/bin/env ruby
+# Copyright 2012  Vincent Batts, Raleigh, NC, USA
+# All rights reserved.
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+# EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=begin
+This is intended to help me script my account access,
+and provide alternate forms of manipulating the data.
+
+
+http://wiki.gnucash.org/wiki/SQL
+see the Entity Relaional Diagram image there
+=end
 
 require 'rubygems'
 require 'mysql'
@@ -11,13 +39,29 @@ ActiveRecord::Base.establish_connection('development')
 module Gnucash
   class Accounts < ActiveRecord::Base; end
   class Books < ActiveRecord::Base; end
+  class Billterms < ActiveRecord::Base; end
   class BudgetAmounts < ActiveRecord::Base; end
+  class Budgets < ActiveRecord::Base; end
   class Commodities < ActiveRecord::Base; end
+  class Customers < ActiveRecord::Base; end
+  class Employees < ActiveRecord::Base; end
+  class Entries < ActiveRecord::Base; end
+  class Gnclock < ActiveRecord::Base
+    set_table_name 'gnclock'
+  end
+  class Invoices < ActiveRecord::Base; end
+  class Jobs < ActiveRecord::Base; end
+  class Lots < ActiveRecord::Base; end
+  class Orders < ActiveRecord::Base; end
+  class Prices < ActiveRecord::Base; end
   class Recurrences < ActiveRecord::Base; end
   class Schedxactions < ActiveRecord::Base; end
   class Slots < ActiveRecord::Base; end
   class Splits < ActiveRecord::Base; end
+  class TaxableEntries < ActiveRecord::Base; end
+  class Taxables < ActiveRecord::Base; end
   class Transactions < ActiveRecord::Base; end
+  class Vendors < ActiveRecord::Base; end
   class Versions < ActiveRecord::Base; end
 end
 
